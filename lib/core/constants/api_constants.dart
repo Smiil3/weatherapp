669de618babed85be7f91ece58fa6c2ab5507ab4
Eduard Baescu <eduard@geev.com>
2026-05-27 16:05:24 +0200

    Init

--- lib/core/constants/api_constants.dart
+++
@@ -1,8 +0,0 @@
-class ApiConstants {
-  static const currentUrl = 'https://api.openweathermap.org/data/2.5/weather';
-  static const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
-  static const iconBaseUrl = 'https://openweathermap.org/img/wn';
-  static const apiKey = String.fromEnvironment('OWM_API_KEY');
-
-  static String iconUrl(String icon) => '$iconBaseUrl/$icon@2x.png';
-}
--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:weatherapp/main.dart';
-
-void main() {
-  testWidgets('WeatherApp smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const WeatherApp());
-    expect(find.byType(WeatherApp), findsOneWidget);
-  });
-}
